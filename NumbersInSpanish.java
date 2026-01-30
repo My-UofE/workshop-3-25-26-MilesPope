9f1d0471a159ca57ec991b0b6d616b4ac0127084
MilesPope <mp1037@exeter.ac.uk>
2026-01-30 12:43:12 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -4,18 +4,37 @@ public class NumbersInSpanish {
         int number = Integer.parseInt(args[0]);
 
         // delete the line below and replace with your code
-        if (number == 1) {
-            System.out.println("uno");
-        } else if ( number == 2 ) {
-            System.out.println("dos");
-        } else if ( number == 3) {
-            System.out.println("tres");
-        } else if ( number == 4 ){
-            System.out.println("cuatro");
-        } else if ( number == 5 ) {
-            System.out.println("cinco");
-        } else {
-            System.out.println("Moron passed in a number that does't work!");
+//        if (number == 1) {
+//            System.out.println("uno");
+//        } else if ( number == 2 ) {
+//            System.out.println("dos");
+//        } else if ( number == 3) {
+//            System.out.println("tres");
+//        } else if ( number == 4 ){
+//            System.out.println("cuatro");
+//        } else if ( number == 5 ) {
+//            System.out.println("cinco");
+//        } else {
+//            System.out.println("Moron passed in a number that does not work!");
+//        }
+        // Using switch
+        switch(number) {
+            case 1:
+                System.out.println( "uno" );
+                break; // don't forget the break after each case!
+            case 2:
+                System.out.println( "dos" );
+                break;
+            case 3:
+                System.out.println("tres");
+                break;
+            case 4:
+                System.out.println("cuatro");
+                break;
+            case 5:
+                System.out.println("cinco");
+            default:
+                System.out.println("Moron passed in a number that does not work!");
         }
     }
 }
